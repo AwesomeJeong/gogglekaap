b52c12b6cf0e627010307bbf659c1a50aaeb0ae1
Awesome Jeong <awesome.dvlp@gmail.com>
2021-11-22 18:11:56 +0900

    11 ch02_01 API 유효성 검사

--- gogglekaap/apis/__init__.py
+++ gogglekaap/apis/__init__.py
@@ -1,9 +1,18 @@
 from flask_restx import Api
-from flask import Blueprint
+from flask import Blueprint, g, abort
 from .user import ns as UserNamespace
 from .item import ns as ItemNamespace
 from .parter import ns as PartnerNamespace
+from functools import wraps
 
+# 데코레이션 패턴 공부하면 알게 될 내용인데 개인적으로 공부해보면 좋을 듯
+def check_session(func):
+    @wraps(func)
+    def __wrapper(*args, **kwargs):
+        if not g.user:
+            abort(401)
+        return func(*args, **kwargs)
+    return __wrapper
 
 blueprint = Blueprint(
     "api",
@@ -16,6 +25,7 @@ api = Api(
     title="gogglekapp API",
     version="1.0",
     doc="/docs",
+    decorators=[check_session],
     description="Welcome my api docs"
 )
 
